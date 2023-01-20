d916eacda6aa4172ca996a4636bc33782702fe58
Mitsuaki Sato <sato@c2c-platform.com>
2023-01-20 12:53:32 +0900

    feature: Add headlessus and heroicons. Create Navbar and BaseLayout components

---
+++ components/Layout/BaseLayout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const BaseLayout = ({ children }: Props) => {
+  return (
+    <>
+      <Navbar />
+      <div className="py-16 bg-gray-50 overflow-hidden min-h-screen">
+        <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BaseLayout;
